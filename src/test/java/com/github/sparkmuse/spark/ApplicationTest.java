2352711549ec3f13639de05ee2fa2a295a2dd355
Alfredo Lopez <alfredlopezf@hotmail.com>
2019-10-08 21:47:09 +0200

    Disable test

--- src/test/java/com/github/sparkmuse/spark/ApplicationTest.java
+++ src/test/java/com/github/sparkmuse/spark/ApplicationTest.java
@@ -1,9 +1,11 @@
 package com.github.sparkmuse.spark;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("We will enable it again when we have an in memory database")
 class ApplicationTest {
     @Test
     void contextLoads() {
